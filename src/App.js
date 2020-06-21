6947f453e76784456510539a574c507322f4aa30
Iván Alcántara <ivanalcantaraweb@gmail.com>
2020-06-21 12:47:01 -0500

    heroku scripts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import './App.css';
-
-
 import Parent from './components/ejercicio1/parent';
 import ApiRest from './components/ejercicio2/apirest';
 import Home from './components/home';
